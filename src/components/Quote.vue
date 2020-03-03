77abf5c4f9dac3afe9928183360b595b14d7301d
bahachammakhi <bahachammakhi25@gmail.com>
2020-03-03 22:31:47 +0100

    inital commit

---
+++ src/components/Quote.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-sm-6 col-md-4 col-lg-3">
+    <div class="card card-default">
+      <div class="card-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.panel-body {
+  font-family: fantasy;
+  font-size: 24px;
+  color: #6e6e6e;
+}
+.quote {
+  cursor: pointer;
+}
+.quote:hover {
+  background-color: #ffe2e2;
+}
+</style>
---
+++ src/views/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes">
+    </app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid
+      :quotes="quotes"
+      @quoteDeleted="deleteQuote"
+    ></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a quote to delete it</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import QuoteGrid from "@/components/QuoteGrid.vue";
+import NewQuote from "@/components/NewQuote.vue";
+import Header from "@/components/Header.vue";
+export default {
+  name: "App",
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  },
+  data: function() {
+    return {
+      quotes: ["Just a Quote to see something"],
+      maxQuotes: 5
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        alert("please delete quotes first !");
+      } else {
+        this.quotes.push(quote);
+      }
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  }
+};
+</script>
+<style>
+.container {
+  overflow: hidden;
+}
+</style>
